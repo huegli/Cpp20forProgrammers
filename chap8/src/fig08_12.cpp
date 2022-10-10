a428bace3c7d2e9488ef8581f52692d9e8a399b2
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2022-10-10 09:15:54 -0700

    New CMake structure, up to fig08_12

---
+++ chap8/src/fig08_12.cpp
@@ -0,0 +1,10 @@
+// fig08_12.cpp
+// Using an ostringstream object.
+#include <iostream>
+#include <sstream> // header for string stream processing
+#include <string>
+
+int main() {
+    std::ostringstream output; // Create ostringstream object
+
+}
