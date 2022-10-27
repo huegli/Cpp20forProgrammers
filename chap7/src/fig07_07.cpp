20aa08cec0c77c2316d50dc3f42b10373ac41a58
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2022-10-27 08:54:00 -0700

    Moved everything to Part1

--- chap7/src/fig07_07.cpp
+++
@@ -1,9 +0,0 @@
-// fig07_07.cpp
-// Attempting to modify data through a 
-// nonconstant pointer to constant data.
-
-int main() {
-    int y{0};
-    const int* yPtr{&y};
-    *yPtr = 100; // error: cannot modify a const object
-}
--- chap8/src/fig08_03.cpp
+++
@@ -1,9 +0,0 @@
-// fig08_03.cpp
-// Demonstrating string member function substr
-#include <iostream>
-#include <string>
-
-int main() {
-    const std::string s{ "airplane" };
-    std::cout << s.substr(3, 4) << '\n'; // retreive substring "plan"
-}
