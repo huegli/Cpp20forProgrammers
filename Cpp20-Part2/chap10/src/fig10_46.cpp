1fe338886c7e7c773413e9f74a5e2f6341359daa
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2022-11-09 09:03:49 -0800

    Completing Chapter 10

---
+++ Cpp20-Part2/chap10/src/fig10_46.cpp
@@ -0,0 +1,47 @@
+// fig10_46.cpp
+// Attempting to polymorphically call a function that is
+// inherited from each of two base classes
+#include <iostream>
+
+// class Base definition
+class Base {
+public:
+    virtual void print() const = 0; // pure virtual
+};
+
+// class DerivedOne definition
+class DerivedOne : public Base {
+public:
+    // override print function
+    void print() const override {std::cout << "DerivedOne\n";}
+};
+
+// class DerivedTwo definition
+class DerivedTwo : public Base {
+public:
+    // overrid print function
+    void print() const override {std::cout << "DerivedTwo\n";}
+};
+
+// class Multiple definition
+class Multiple : public DerivedOne, public DerivedTwo {
+public:
+    // qualify which version of function print
+    void print () const override {DerivedTwo::print();}
+};
+
+int main() {
+    Multiple both{}; // instantiate a Multiple object
+    DerivedOne one{}; // instantiate a DerivedOne object
+    DerivedTwo two{}; // instantiate a DerivedTwo object
+    Base* array[3]{}; // create array of base-class pointers
+
+    array[0] = &both; // ERROR--ambiguous
+    array[1] = &one;
+    array[2] = &two;
+
+    // polymorphically invoke print
+    for (int i{0}; i < 3; ++i) {
+        array[i]->print();
+    }
+}
